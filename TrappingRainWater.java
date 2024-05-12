16f8b4c1293a524678e57c03b75e23e63f04e1cb
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2024-05-12 19:08:48 +0530

    Trapping Rain Water

--- TrappingRainWater.java
+++ TrappingRainWater.java
@@ -1,3 +1,4 @@
+//Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
