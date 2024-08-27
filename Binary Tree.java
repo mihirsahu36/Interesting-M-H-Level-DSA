ab5319decb1756efdaaf7788e5b577c9f0d055af
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2024-08-28 01:17:18 +0530

    Update Binary Tree.java

--- Binary Tree.java
+++ Binary Tree.java
@@ -64,3 +64,22 @@ class Solution {
         return Math.max(left, right) + 1;
     }
 }
+
+
+//112. Path Sum
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return dfs(root, targetSum);
+    }
+
+    private boolean dfs(TreeNode root, int s) {
+        if (root == null) {
+            return false;
+        }
+        s -= root.val;
+        if (root.left == null && root.right == null && s == 0) {
+            return true;
+        }
+        return dfs(root.left, s) || dfs(root.right, s);
+    }
+}
