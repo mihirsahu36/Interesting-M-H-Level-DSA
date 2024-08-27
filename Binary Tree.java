aa14d51dd479a208d3ce40f6d7d5c07f49b3de6f
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2024-08-28 01:29:44 +0530

    Update Binary Tree.java

--- Binary Tree.java
+++ Binary Tree.java
@@ -83,3 +83,29 @@ class Solution {
         return dfs(root.left, s) || dfs(root.right, s);
     }
 }
+
+
+//113. Path Sum II
+class Solution {
+    private List<List<Integer>> ans = new ArrayList<>();
+    private List<Integer> m = new ArrayList<>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        dfs(root, targetSum);
+        return ans;
+    }
+
+    private void dfs(TreeNode root, int s) {
+        if (root == null) {
+            return;
+        }
+        s -= root.val;
+        m.add(root.val);
+        if (root.left == null && root.right == null && s == 0) {
+            ans.add(new ArrayList<>(m));
+        }
+        dfs(root.left, s);
+        dfs(root.right, s);
+        m.remove(m.size() - 1);
+    }
+}
