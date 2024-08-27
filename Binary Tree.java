fc13ce9be5131d02ee5e861133f0355cf66d4015
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2024-08-28 02:19:42 +0530

    Update Binary Tree.java

--- Binary Tree.java
+++ Binary Tree.java
@@ -152,3 +152,22 @@ class Solution {
         return dfs(root.right);
     }
 }
+
+
+//979. Distribute Coins in Binary Tree
+class Solution {
+    private int res;
+    public int distributeCoins(TreeNode root) {
+        balance(root);
+        return res;
+    }
+    private int balance(TreeNode node){
+        if(node == null) return 0;
+        else{
+            int left = balance(node.left);
+            int right = balance(node.right);
+            res += Math.abs(left) + Math.abs(right);
+            return node.val - 1 + left + right;
+        }
+    }
+}
