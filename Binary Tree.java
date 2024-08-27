e4d4fd7235ccdf5ab580d3ebda420d1a5ff2dee7
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2024-08-28 01:42:30 +0530

    Update Binary Tree.java

--- Binary Tree.java
+++ Binary Tree.java
@@ -109,3 +109,22 @@ class Solution {
         m.remove(m.size() - 1);
     }
 }
+
+
+//94. Binary Tree Inorder Traversal
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorderHelper(root, result);
+        return result;
+    }
+
+    private void inorderHelper(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        inorderHelper(root.left, result);
+        result.add(root.val);
+        inorderHelper(root.right, result);
+    }
+}
