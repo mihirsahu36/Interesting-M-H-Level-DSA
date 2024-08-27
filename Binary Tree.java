0003d35622bc283d912581c265d17187c5046840
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2024-08-28 01:02:28 +0530

    Update Binary Tree.java

--- Binary Tree.java
+++ Binary Tree.java
@@ -26,3 +26,22 @@ class Solution {
         return Math.max(leftDepth, rightDepth) + 1;
     }
 }
+
+
+//111. Minimum Depth of Binary Tree
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        }
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+        int leftDepth=minDepth(root.left);
+        int rightDepth=minDepth(root.right);
+        return Math.min(leftDepth, rightDepth) + 1;
+    }
+}
